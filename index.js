744ebdc44a1d5eea062a62c7d2e0eec29c65b8f2
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-09-05 19:22:34 -0300

    Exercicios JS

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 window.alert('Tudo certin?')
+
+//1
+
+var idadeAtual = prompt('Digite sua idade atual');
+idadeAtual = parseInt(idadeAtual);
+var idadeFutura = idadeAtual + 30;
+alert('Sua idade atual é: ' + idadeAtual);
+alert('Sua idade daqui a 30 anos será: ' + idadeFutura);
+
+
+
+//3
+
+var nome = prompt('Digite seu nome antes de entrar no site');
+if (nome) {
+    var idade = prompt('Digite sua idade para realizar a votação');
+    if (idade && parseInt(idade) >= 18) {
+        alert('Bem vindo, sua idade e legal ' + nome+ '!');
+    } else {
+        alert('Desculpe, você ainda nao pode votar.');
+    }
+}
