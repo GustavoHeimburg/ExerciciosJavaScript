76f974e76aed61dff2fff9e8369ba0e419626153
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-09-05 19:34:59 -0300

    Exercicios JS

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 window.alert('Tudo certin?')
 
 //1
-
-var idadeAtual = prompt('Digite sua idade atual');
-idadeAtual = parseInt(idadeAtual);
-var idadeFutura = idadeAtual + 30;
-alert('Sua idade atual é: ' + idadeAtual);
-alert('Sua idade daqui a 30 anos será: ' + idadeFutura);
-
-
-
-//3
-
 var nome = prompt('Digite seu nome antes de entrar no site');
-if (nome) {
-    var idade = prompt('Digite sua idade para realizar a votação');
-    if (idade && parseInt(idade) >= 18) {
-        alert('Bem vindo, sua idade e legal ' + nome+ '!');
+var idadeAtual = prompt('Digite sua idade atual');
+
+idadeAtual = parseInt(idadeAtual);
+alert(nome+', Sua idade atual é: ' + idadeAtual);
+alert(nome+', Daqui a 30 anos você terá: ' + (idadeAtual + 30 * 365) + " dias de vida");
+
+
+    if (parseInt(idadeAtual) >= 16) {
+        alert('Na sua idade atual, você está apto a votar ' + nome+ '!');
     } else {
-        alert('Desculpe, você ainda nao pode votar.');
+        alert('Desculpe' +nome+', na sua idade atual, você ainda não está apto para votar.');
     }
-}
+
+idadeAtual = idadeAtual * 3;
+
+    if(idadeAtual >= 18){
+        alert(nome+", Daqui a 40 meses, você será maior de idade, com: " + idadeAtual +" anos.");
+    }else{
+        alert(nome+", Daqui a 40 meses, você não será maior de idade, e terá: " + idadeAtual +" anos");
+    }
