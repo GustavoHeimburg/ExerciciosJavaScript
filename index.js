1c920b6f158aa84a30733f7b57cc701a2ec8f027
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-09-05 20:12:15 -0300

    Exercicios JS

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-window.alert('Tudo certin?')
+document.getElementById("calc").onclick = function() {
+    window.alert('Tudo certin?')
 
 //1
-var nome = prompt('Digite seu nome antes de entrar no site');
-var idadeAtual = prompt('Digite sua idade atual em anos');
-var meses = prompt("Digite em qual mes você faz aniversario");
+    var nome = prompt('Digite seu nome antes de entrar no site');
+    var idadeAtual = prompt('Digite sua idade atual em anos');
+    var meses = prompt("Digite em qual mes você faz aniversario");
 
-idadeAtual = parseInt(idadeAtual);
-alert(nome+', Sua idade atual é: ' + idadeAtual);
-alert(nome+', Daqui a 30 anos você terá: ' + (idadeAtual + 30 * 365) + " dias de vida");
+    idadeAtual = parseInt(idadeAtual);
+    alert(nome + ', Sua idade atual é: ' + idadeAtual);
+    alert(nome + ', Daqui a 30 anos você terá: ' + (idadeAtual + 30 * 365) + " dias de vida");
 
 
     if (parseInt(idadeAtual) >= 16) {
-        alert('Na sua idade atual, você está apto a votar ' + nome+ '!');
+        alert('Na sua idade atual, você está apto a votar ' + nome + '!');
     } else {
-        alert('Desculpe' +nome+', na sua idade atual, você ainda não está apto para votar.');
+        alert('Desculpe' + nome + ', na sua idade atual, você ainda não está apto para votar.');
     }
-if(meses >= 8){
-    idadeAtual = idadeAtual + 4;
-}else{
-    idadeAtual = idadeAtual + 3;
+    if (meses >= 8) {
+        idadeAtual = idadeAtual + 4;
+    } else {
+        idadeAtual = idadeAtual + 3;
+    }
+
+    if (idadeAtual >= 18) {
+        alert(nome + ", Daqui a 40 meses, você será maior de idade, com: " + idadeAtual + " anos.");
+    } else {
+        alert(nome + ", Daqui a 40 meses, você não será maior de idade, e terá: " + idadeAtual + " anos");
+    }
+}
+document.getElementById("boot").onclick = function() {{
+    var nome = prompt('Digite seu nome antes de entrar no site');
+    alert(`Obrigado por entrar ${nome}`)
+    var peso = prompt('Digite seu peso')
+    var altura = prompt('Digite sua altura')
+    const imc = peso / (altura * altura)
+if (imc >=30){
+    alert(`Seu IMC esta alterado, tome cuidado ${nome} ${imc}`)
+    }else {
+    alert(`Seu IMC esta favoravel com suas caracteristicas corporais` + nome + " imc:"+ imc )
+}
+}
 }
 
-    if(idadeAtual >= 18){
-        alert(nome+", Daqui a 40 meses, você será maior de idade, com: " + idadeAtual +" anos.");
-    }else{
-        alert(nome+", Daqui a 40 meses, você não será maior de idade, e terá: " + idadeAtual +" anos");
-    }
+
+
+
